52d7d365a56b80ecda34986941a187d4f1dfd281
unknown <best@gamma-fusion-lt.cs.unc.edu>
2014-03-06 13:22:15 -0500

    added stream operator to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,58 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include "Vector3.h"
 
-struct Vector3{
-    float x;
-	float y;
-	float z;
 
-	friend std::istream& operator>>(std::istream& in, Vector3& vec);
-	friend std::ifstream& operator >> (std::ifstream& in, Vector3& vec);
-};
 
 
 
 int main(){
 
 	Vector3 ted;
-	int blah;
 	std::string test;
 	std::ifstream fin;
-	fin.open("xyz.txt");	
+	fin.open("..\\..\\data\\membrane_xyz.txt");	
+	float maxx, maxy, maxz, minx, miny, minz;
+	maxx = maxy = maxz = -10000;
+	minx = miny = minz = 10000;
+	std::vector<Vector3> vectorList;
+	vectorList.reserve(15000);
 
+	
 	if (fin.is_open()) {
 
 		//fin >> test;
-		fin >> ted;
-		std::cout << ted.x << " " << ted.y << " " << ted.z << "\n";
+		while (!fin.eof()){   
+			fin >> ted;
+			//std::cout << ted.x << " " << ted.y << " " << ted.z << "\n";
 		
-		fin >> ted;
-		std::cout << ted.x << " " << ted.y << " " << ted.z << "\n";
-		
-		//std::cout << test;
+			if (ted.x > maxx)
+				maxx = ted.x;
+			else if (ted.x < minx)
+				minx = ted.x;
+
+			if (ted.y > maxy)
+				maxy = ted.y;
+			else if (ted.y < miny)
+				miny = ted.y;
+
+			if (ted.z > maxz)
+				maxz = ted.z;
+			else if (ted.z < minz)
+				minz = ted.z;
+
+			vectorList.push_back(ted);
+		}
+		std::cout <<"MAX " << maxx << " min " << minx << std::endl;
 	}
 	else {
 		return 1;
 	}
+	std::cout <<"SIZE " << vectorList.size() << std::endl;
 	fin.close();
 	return 0;
 }
 
 
-std::istream& operator>>(std::istream& in, Vector3& vec)
-{
-    in >> vec.x >> vec.y >> vec.z;
-	return in;
-}
-
-std::ifstream& operator >> (std::ifstream& in, Vector3& vec)
-{
-    in >> vec.x >> vec.y >> vec.z;
-	return in;
-}
