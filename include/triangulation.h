78e947b289b3798869b49fb8e882330101afbb06
Shawn Waldon <swaldon@cs.unc.edu>
2014-03-06 13:50:34 -0500

    Updated triangulation interface.

--- include/triangulation.h
+++ include/triangulation.h
@@ -3,19 +3,16 @@
 
 #include <vector>
 
-typedef X Triangle;
 typedef Y Point;
 typedef Z Edge;
 
 namespace Triangulation {
 
-void triangulate(Triangle* boundingFace, const std::vector<Point>& pts);
+void triangulate(Edge* boundingFace, const std::vector<Point>& pts);
 
-void addPoint(Triangle* currentTris, const Point& pt);
+void addPoint(Edge* currentTris, const Point& pt);
 
-Triangle* Triangulation::localizePoint(Triangle* currentTris, const Point& pt);
-
-void splitTriangle(Triangle* tri, const Point& ptInTri);
+void splitTriangleToLeft(Edge* tri, const Point& ptInTri);
 
 void Triangulation::questionAndSwapEdges(std::vector<Edge*>& edges, const Point& newPoint);
 
