0027b2e86ea0df77229cdcce2701449a7ce193ee
N Morales <n.morales.0@gmail.com>
2014-03-25 13:02:13 -0400

    removed vs files, added some initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
-/*#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "Vector3.h"
 
+using namespace std;
 
-int main(){
-
+int main()
+{
 	Vector3 ted;
 	std::string test;
 	std::ifstream fin;
@@ -34,23 +36,28 @@ int main(){
 				maxy = ted.y;
 			else if (ted.y < miny)
 				miny = ted.y;
-
+			/*
 			if (ted.z > maxz)
 				maxz = ted.z;
 			else if (ted.z < minz)
-				minz = ted.z;
+				minz = ted.z;*/
 
 			vectorList.push_back(ted);
 		}
 		std::cout <<"MAX " << maxx << " min " << minx << std::endl;
-	}
-	else {
+	} else {
 		return 1;
 	}
 	std::cout <<"SIZE " << vectorList.size() << std::endl;
+	
+	float dmax = max( maxx - minx, maxy - miny );
+	dmax = max( dmax, 10.0f );	// in case of a single point
+	float cx = ( maxx - minx ) * 0.5f;
+	float cy = ( maxy - miny ) * 0.5f;
+
+
+	
 	fin.close();
 	return 0;
 }
 
-
-*/
