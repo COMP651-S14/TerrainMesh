b542e5030db39c5572a610e952ddc1ed8e552511
Shawn Waldon <swaldon@cs.unc.edu>
2014-03-27 13:48:20 -0400

    Added code to split triangle into 3 when new point added inside it

--- src/triangulation.cxx
+++ src/triangulation.cxx
@@ -2,7 +2,7 @@
 #include "triangulation.h"
 #include "cell.h"
 
-Face* localizePoint(Cell*, const Point&);
+Edge* localizePoint(Cell*, const Point&);
 
 void splitTriangleToLeft(Cell* wrapper, Edge* tri,
                          const Point& ptInTri);
@@ -19,8 +19,18 @@ void Triangulation::triangulate(Cell* boundingFace,
   }
 }
 
-void Triangulation::addPoint(Cell* currentTris, const Point& pt)
+void Triangulation::addPoint(Cell* c, const Point& pt)
 {
+    Edge *e1 = localizePoint(c,pt);
+    Vertex *v1 = e1->Origin(), *v2 = e1->Dest();
+    Face *f = e1->getLeft();
+    Edge *e2 = e1->Lnext();
+    Edge *e3 = e2->Lprev();
+    Vertex *v3 = e2->Dest();
+    Face *f2 = c->makeFaceEdge(f,v1,v2);
+    Vertex *vnew = c->makeVertexEdge(v2,f,f2);
+    c->makeFaceEdge(f,vnew,v3);
+    // TODO - finish by testing neighbors
 }
 
 void splitTriangleToLeft(Cell* wrapper, Edge* tri,
