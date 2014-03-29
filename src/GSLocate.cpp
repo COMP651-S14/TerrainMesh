3a8fbe6a69cf3a1bb2ddc6d42947bb8f7e8774a1
sahil <sahil@cs.unc.edu>
2014-03-29 15:48:10 -0400

    Added GSLocate

---
+++ src/GSLocate.cpp
@@ -0,0 +1,42 @@
+
+#include "GSLocate.h"
+
+#include <predicates.h>
+#include "edge.h"
+/*  orient2dfast()   Approximate 2D orientation test.  Nonrobust.            */
+/*  orient2dexact()   Exact 2D orientation test.  Robust.                    */
+/*  orient2dslow()   Another exact 2D orientation test.  Robust.             */
+/*  orient2d()   Adaptive exact 2D orientation test.  Robust.                */
+/*                                                                           */
+/*               Return a positive value if the points pa, pb, and pc occur  */
+/*               in counterclockwise order; a negative value if they occur   */
+/*               in clockwise order; and zero if they are collinear.  The    */
+/*               result is also a rough approximation of twice the signed    */
+/*               area of the triangle defined by the three points.           */                                    
+
+
+Edge* gsLocate(vec3 x, Edge* initEdge){
+	Edge * e=initEdge;
+	while(true){
+		if(x==e->Org->pos  || x==e->Dest->pos)
+			return e;
+		else
+		{
+			if(orient2dfastVec(e->Org->pos,e->Dest->pos,x)==-1) 
+				e=e->Sym();
+			else
+			{
+				if(orient2dfastVec(e->Onext()->Org->pos,e->Onext()->Dest->pos,x)!=-1)
+					e=e->Onext();
+				else
+				{
+					if(orient2dfastVec(e->Dprev()->Org->pos,e->Dprev()->Dest->pos,x)!=-1)
+						e=e->Dprev();
+					else
+						return e;
+				}
+			}
+		}
+
+	}
+}
---
+++ src/GSLocate.h
@@ -0,0 +1,11 @@
+#include "vertex.h"
+
+class Edge;
+
+/*
+	@brief:        Locate edge containing or bounding a point	
+	@param		X			point whose location has to be found
+	@returns :     Returns the edge on which point x lies or which has the trangle containing X on its left
+*/
+Edge* gsLocate(vec3 x, Edge* initEdge); 
+
